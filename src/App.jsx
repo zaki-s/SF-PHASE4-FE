bf1c5d5063916f8fa2fe7c0bf0e79812e6329930
zaki-s <zakijames024@gmail.com>
2025-06-27 15:32:32 +0300

    Initialising prog

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Skill Forge</h1>
+      <p>Your journey to mastering skills starts here!</p>
+    </div>
+  );  
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+// styles will be in individual files in folder called styles
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
