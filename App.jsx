ef9a6010138493161e71c1a01f2bee5000cb9f22
leonard185 <61781922+leonard185@users.noreply.github.com>
2025-06-30 07:13:40 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RegisterForm from './forms/RegisterForm';
-import LoginForm from './forms/LoginForm';
-import ResetPassword from './forms/ResetPassword';
-import Logout from './forms/Logout';
-const App = () => {
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage.jsx";
+import JourneyPage from './pages/JourneyPage.jsx';
+import MyProgress from './pages/MyProgress.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import RegisterForm from './Auth/RegisterForm.jsx';
+import LoginForm from './Auth/LoginForm.jsx';
+import Logout from './Auth/Logout.jsx';
+import ResetPassword from './Auth/ResetPassword.jsx';
+
+
+
+
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/reset" element={<ResetPassword />} />
-        <Route path="/" element={<RegisterForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/logout" element={<Logout />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/journey" element={<JourneyPage />} />
+        <Route path="/progress" element={<MyProgress />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path='/register' element={<RegisterForm />} />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='/reset' element={<ResetPassword />} />
         
       </Routes>
-    </BrowserRouter>
-  );
-};
+    </Router>
+  )
+}
 
 export default App;
